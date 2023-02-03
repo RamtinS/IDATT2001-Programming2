de35781a7704ee6c5d2426e20e02738d4719986f
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-02-03 18:01:43 +0100

    First draft of the game class. It contains some Errors, because it is dependent on the Goal class which is missing.

---
+++ src/main/java/edu/ntnu/idatt2001/paths/Game.java
@@ -0,0 +1,44 @@
+package edu.ntnu.idatt2001.paths;
+
+import java.util.Objects;
+
+/**
+ * The class represents a game.
+ *
+ * @author ...
+ * @version JDK 17
+ */
+public class Game {
+  private final Player player;
+  private final Story story;
+  private final List<Goal> goals;
+
+  public Game(Player player, Story story, List<Goal> goals) throws NullPointerException {
+    Objects.requireNonNull(player, "\nPlayer cannot be null");
+    Objects.requireNonNull(story, "\nStory cannot be null");
+    this.player = player;
+    this.story = story;
+    this.goals = goals;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public Story getStory() {
+    return story;
+  }
+
+  public List<Goal> getGoals() {
+    return goals;
+  }
+
+  public Passage begin() {
+    return story.getOpeningPassage();
+  }
+
+  public Passage go(Link link) throws NullPointerException{
+    Objects.requireNonNull(link, "\nLink cannot be null");
+    return story.getPassage(link);
+  }
+}
