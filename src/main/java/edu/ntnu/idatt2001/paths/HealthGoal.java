dfd503193778cde92310b38b38c57b34ad925597
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-02-04 12:32:06 +0100

    Added Javadoc to the HealthGoal class.

--- src/main/java/edu/ntnu/idatt2001/paths/HealthGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/HealthGoal.java
@@ -2,18 +2,42 @@ package edu.ntnu.idatt2001.paths;
 
 import java.util.Objects;
 
+/**
+ * The class represents an expected minimum health value.
+ *
+ * @author ...
+ * @version JDK 17
+ */
 public class HealthGoal implements Goal{
   private final int minimumHealth;
 
+  /**
+   * Constructor to create an object of the type HealthGoal.
+   *
+   * @param minimumHealth minimum health value.
+   * @throws IllegalArgumentException if minimumHealth is less than zero.
+   */
   public HealthGoal(int minimumHealth) throws IllegalArgumentException{
     if (minimumHealth < 0) throw new IllegalArgumentException("\nMinimum health cannot be less than zero.");
     this.minimumHealth = minimumHealth;
   }
 
+  /**
+   * The method retrieves the minimum health value.
+   *
+   * @return the minimum health value.
+   */
   public int getMinimumHealth() {
     return minimumHealth;
   }
 
+  /**
+   * The method checks if the minimum health value is achieved.
+   *
+   * @param player the player assigned to the goal.
+   * @return true or false depending on whether the goal is achieved.
+   * @throws NullPointerException if the player is null.
+   */
   @Override
   public boolean isFulfilled(Player player) throws NullPointerException {
     Objects.requireNonNull(player, "\nPlayer cannot be null.");
