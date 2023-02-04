774cf03397708223e2cfb97ac044bf8e689366a2
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-02-04 12:19:26 +0100

    Added Javadoc to the GoldGoal class.

--- src/main/java/edu/ntnu/idatt2001/paths/GoldGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/GoldGoal.java
@@ -2,18 +2,42 @@ package edu.ntnu.idatt2001.paths;
 
 import java.util.Objects;
 
+/**
+ * The class represents an expected minimum gold holding.
+ *
+ * @author ...
+ * @version JDK 17
+ */
 public class GoldGoal implements Goal{
   private final int minimumGold;
 
+  /**
+   * Constructor to create an object of the type GoldGoal.
+   *
+   * @param minimumGold a minimum gold holding.
+   * @throws IllegalArgumentException if minimumGold is less than zero.
+   */
   public GoldGoal(int minimumGold) throws IllegalArgumentException {
     if (minimumGold < 0) throw new IllegalArgumentException("\nMinimum gold cannot be less than zero.");
     this.minimumGold = minimumGold;
   }
 
+  /**
+   * The method retrieves the minimum amount of gold.
+   *
+   * @return the minimum amount of gold.
+   */
   public int getMinimumGold() {
     return minimumGold;
   }
 
+  /**
+   * The method checks if the minimum gold goal is achieved.
+   *
+   * @param player the player assigned to the goal.
+   * @return true or false depending on whether the goal is achieved.
+   * @throws NullPointerException if the player is null.
+   */
   @Override
   public boolean isFulfilled(Player player) throws NullPointerException {
     Objects.requireNonNull(player, "\nPlayer cannot be null.");
