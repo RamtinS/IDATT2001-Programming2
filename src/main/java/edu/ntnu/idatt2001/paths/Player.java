51da587970bc3f72a202f3256514f84d2608a952
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-02-04 15:08:57 +0100

    Added JavaDoc to the Player class

--- src/main/java/edu/ntnu/idatt2001/paths/Player.java
+++ src/main/java/edu/ntnu/idatt2001/paths/Player.java
@@ -12,70 +12,132 @@ import java.util.Objects;
  * @version JDK 17
  */
 public class Player {
-    private final String name;
-    private int health;
-    private int score;
-    private int gold;
-    private final List<String> inventory;
+  private final String name;
+  private int health;
+  private int score;
+  private int gold;
+  private final List<String> inventory;
 
 
+  /**
+   * Constructor for an object of the Player class.
+   *
+   * @param name The name of the player.
+   * @param health The starting health of the player.
+   * @param score The starting score of the player.
+   * @param gold The amount of gold the player starts with.
+   * @throws NullPointerException If the name of the player is null.
+   * @throws IllegalArgumentException If the health is not greater than zero, if the score
+   *        is a negative number or if the amount of gold is a negative number.
+   */
+  public Player(String name, int health, int score, int gold)
+          throws NullPointerException, IllegalArgumentException {
+    Objects.requireNonNull(name, "\nName cannot be null");
 
+    if (health <= 0) throw new IllegalArgumentException("\nHealth has to be greater than 0");
+    if (score < 0) throw new IllegalArgumentException("\nScore cannot be a negative number");
+    if (gold < 0) throw new IllegalArgumentException("\n gold cannot be a negative number");
 
-    public Player(String name, int health, int score, int gold) throws NullPointerException, IllegalArgumentException {
-        Objects.requireNonNull(name, "\nName cannot be null");
+    this.name = name;
+    this.health = health;
+    this.score = score;
+    this.gold = gold;
+    this.inventory = new ArrayList<>();
+  }
 
-        if (health <=0) throw new IllegalArgumentException("\nHealth has to be greater than 0");
-        if (score < 0) throw new IllegalArgumentException("\nScore cannot be a negative number");
-        if (gold < 0) throw new IllegalArgumentException("\n gold cannot be a negative number");
+  /**
+   * Retrieves the name of the player and returns it.
+   *
+   * @return The name of the player.
+   */
+  public String getName() {
+    return this.name;
+  }
 
-        this.name = name;
-        this.health = health;
-        this.score = score;
-        this.gold = gold;
-        this.inventory = new ArrayList<>();
-    }
+  /**
+   * Adds health to the player.
+   *
+   * @param health The amount of health to be added to the player.
+   * @throws IllegalArgumentException If the change in health is not greater
+   *        than 0.
+   */
+  public void addHealth(int health) throws IllegalArgumentException {
+    if (health <= 0) throw new IllegalArgumentException("Health increase has"
+            + " to be a positive number");
+    this.health += health;
+  }
 
-    public String getName(){
-        return this.name;
-    }
+  /**
+   * Retrieves the health of the player and returns it.
+   *
+   * @return The health of the player.
+   */
+  public int getHealth() {
+    return health;
+  }
 
-    public void addHealth(int health) throws IllegalArgumentException{
-        if (health <= 0) throw new IllegalArgumentException("Health increase has to be a positive number");
-        this.health += health;
-    }
+  /**
+   * Adds points to the score of the player.
+   *
+   * @param points The amount of points to be added.
+   * @throws IllegalArgumentException If the point increase is not a positive number.
+   */
+  public void addScore(int points) throws IllegalArgumentException {
+    if (points <= 0) throw new IllegalArgumentException("Point increase has to be positive");
+    this.score += points;
+  }
 
-    public int getHealth(){
-        return health;
-    }
+  /**
+   * Retrieves the score of the player and returns it.
+   *
+   * @return The current score of the player.
+   */
+  public int getScore() {
+    return score;
+  }
 
-    public void addScore(int points) throws IllegalArgumentException{
-        if (points <= 0) throw new IllegalArgumentException("Point increase has to be positive");
-        this.score += points;
-    }
+  /**
+   * Adds gold to the amount of gold the player has.
+   *
+   * @param gold The amount of gold to be added.
+   * @throws IllegalArgumentException If the increase of gold is not a positive number.
+   */
+  public void addGold(int gold) throws IllegalArgumentException {
+    if (gold <= 0) throw new IllegalArgumentException("\nGold increase has to be positive.");
+    this.gold += gold;
+  }
 
-    public int getScore(){
-        return score;
-    }
+  /**
+   * Retrieves the amount of gold the player has and returns it.
+   *
+   * @return The amount of gold that the player has.
+   */
+  public int getGold() {
+    return gold;
+  }
 
-    public void addGold(int gold) throws IllegalArgumentException{
-        if (gold <= 0) throw new IllegalArgumentException("\nGold increase has to be positive.");
-        this.gold += gold;
-    }
+  /**
+   * Adds an item to the inventory of the player.
+   *
+   * @param item The item to be added to the inventory.
+   * @throws NullPointerException If the item is null.
+   * @throws IllegalArgumentException If the String describing the item is blank.
+   */
+  public void addToInventory(String item) throws NullPointerException, IllegalArgumentException {
+    Objects.requireNonNull(item, "Item cannot be null");
+    if (item.isBlank()) throw new IllegalArgumentException("Item cannot be blank");
 
-    public int getGold(){
-        return gold;
-    }
+    inventory.add(item);
+  }
 
-    public void addToInventory(String item) throws NullPointerException, IllegalArgumentException{
-        Objects.requireNonNull(item, "Item cannot be null");
-        if (item.isBlank()) throw new IllegalArgumentException("Item cannot be blank");
-
-        inventory.add(item);
-    }
-
-    public List<String> getInventory(){
-        return inventory;
-    }
+  /**
+   * Retrieves the inventory of a player.
+   *
+   * @return The inventory of a player in the form of a list.
+   */
+  public List<String> getInventory() {
+    return inventory;
+  }
 
 
 
