ab54856a394c757da11af48a1714d0cf1b8151d3
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-02-04 12:33:51 +0100

    Added JavaDoc to the InventoryAction class.
    Added a getItem method, and added the use of this method in the execute method.

--- src/main/java/edu/ntnu/idatt2001/paths/InventoryAction.java
+++ src/main/java/edu/ntnu/idatt2001/paths/InventoryAction.java
@@ -2,15 +2,38 @@ package edu.ntnu.idatt2001.paths;
 
 import java.util.Objects;
 
-public class InventoryAction {
+/**
+ * The class represents an inventory action.
+ */
+public class InventoryAction implements Action {
     private final String item;
 
+    /**
+     * Constructor for an InventoryAction object.
+     * @param item The item that will be awarded to the player completing the action.
+     * @throws NullPointerException If the item given is null.
+     */
     public InventoryAction(String item) throws NullPointerException{
         Objects.requireNonNull(item);
         this.item = item;
     }
 
+    /**
+     * Performs the action, adding the item to the inventory of
+     * the specified player.
+     *
+     * @param player The player that will receive the item.
+     */
+    @Override
     public void execute(Player player){
-        player.addToInventory(item);
+        player.addToInventory(getItem());
+    }
+
+    /**
+     * Gets the item given by the InventoryAction.
+     * @return The item that will be awarded for completing the action.
+     */
+    public String getItem() {
+        return item;
     }
 }
