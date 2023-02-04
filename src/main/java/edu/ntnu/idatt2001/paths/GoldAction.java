92cb16be04e673b5c78146b40f90f6af3eeac36e
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-02-04 11:59:46 +0100

    Added javadoc and added a getPoints() method for the GoldAction class.
    - Removed exception for the constructor, so that the action can include negative values.

--- src/main/java/edu/ntnu/idatt2001/paths/GoldAction.java
+++ src/main/java/edu/ntnu/idatt2001/paths/GoldAction.java
@@ -1,15 +1,36 @@
 package edu.ntnu.idatt2001.paths;
 
-public class GoldAction {
+/**
+ * The class represents a gold action.
+ */
+public class GoldAction implements Action{
     private final int gold;
 
-    public GoldAction(int gold) throws IllegalArgumentException{
-        if (gold <=0) throw new IllegalArgumentException("Gold amount has to be larger than 0");
+    /**
+     * Constructor to create an object of GoldAction
+     *
+     * @param gold The gold that will be awarded for completing the task
+     */
+    public GoldAction(int gold){
         this.gold = gold;
     }
 
-    public void execute(Player player){
-        player.addGold(gold);
+    /**
+     * Executes the gold action on the given player.
+     *
+     * @param player The player that the action will be performed on.
+     */
+    @Override
+    public void execute(Player player) {
+        player.addGold(getGold());
+    }
+
+    /**
+     * Gets the amount of gold given by completing the task.
+     * @return The amount of gold given by completing the task.
+     */
+    public int getGold(){
+        return gold;
     }
 
 
