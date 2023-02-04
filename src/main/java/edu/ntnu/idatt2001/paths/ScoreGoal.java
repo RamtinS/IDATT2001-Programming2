352d0eb1e0e3bda8829a8b0b4025011a7e54c58d
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-02-04 10:47:05 +0100

    Added throws IllegalArgumentException next to the constructor in the ScoreGoal class, to indicate that it is throwing an exception. Added exception handling to the isFulfilled method in the ScoreGoal class.

--- src/main/java/edu/ntnu/idatt2001/paths/ScoreGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/ScoreGoal.java
@@ -1,9 +1,11 @@
 package edu.ntnu.idatt2001.paths;
 
+import java.util.Objects;
+
 public class ScoreGoal implements Goal{
   private final int minimumPoints;
 
-  public ScoreGoal(int minimumPoints) {
+  public ScoreGoal(int minimumPoints) throws IllegalArgumentException {
     if (minimumPoints < 0) throw new IllegalArgumentException("\nMinimum points cannot be less than zero.");
     this.minimumPoints = minimumPoints;
   }
@@ -13,7 +15,8 @@ public class ScoreGoal implements Goal{
   }
 
   @Override
-  public boolean isFulfilled(Player player) {
+  public boolean isFulfilled(Player player) throws NullPointerException {
+    Objects.requireNonNull(player, "\nPlayer cannot be null.");
     return player.getScore() > getMinimumPoints();
   }
 }
