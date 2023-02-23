eef19935d68bcebf9b18b63525be00f8f93ae241
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-02-23 21:34:32 +0100

    Added negative unit tests for the method execute and the constructor in the ScoreAction class. Did also make some small improvements to the test methods.

--- src/test/java/edu/ntnu/idatt2001/paths/actions/ScoreActionTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/actions/ScoreActionTest.java
@@ -22,22 +22,27 @@ class ScoreActionTest {
   }
 
   @Test
+  @DisplayName("Test constructor valid input")
+  void testConstructorValidInput() {
+    ScoreAction scoreActionTest = new ScoreAction(10);
+    int actual = scoreActionTest.getPoints();
+    int expected = 10;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("Test constructor invalid input throws IllegalArgumentException")
+  void testConstructorInvalidInput() {
+    int invalidPoints = -10;
+    assertThrows(IllegalArgumentException.class, () -> new ScoreAction(invalidPoints));
+  }
+
+  @Test
   @DisplayName("Should get points")
   void shouldGetPoints() {
     int expected = 10;
     int actual = scoreAction.getPoints();
-
-    assertEquals(actual, expected);
-  }
-
-  @Test
-  @DisplayName("Test constructor valid input")
-  void testConstructorValidInput(){
-    ScoreAction scoreActionTest = new ScoreAction(10);
-    int actual = scoreActionTest.getPoints();
-    int expected = 10;
-
-    assertEquals(actual, expected);
+    assertEquals(expected, actual);
   }
 
   @Test
@@ -46,7 +51,13 @@ class ScoreActionTest {
     scoreAction.execute(player);
     int expected = 30;
     int actual = player.getScore();
+    assertEquals(expected, actual);
+  }
 
-    assertEquals(actual, expected);
+  @Test
+  @DisplayName("Should not execute throws NullPointerException")
+  void shouldNotExecuteThrowsNullPointerException() {
+    Player notValidPlayer = null;
+    assertThrows(NullPointerException.class, () -> scoreAction.execute(notValidPlayer));
   }
 }
