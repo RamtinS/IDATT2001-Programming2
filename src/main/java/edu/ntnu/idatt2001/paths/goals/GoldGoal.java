d1e8675c364c56adcbb096d3dfb9d762933a23c3
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-04-23 13:09:58 +0200

    Override equals and hashCode in all goal classes
    Override the equals and hashCode methods, so that it is possible to check for similarity between the objects.

--- src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
@@ -1,12 +1,16 @@
 package edu.ntnu.idatt2001.paths.goals;
 
 import edu.ntnu.idatt2001.paths.Player;
+import java.util.Objects;
 
 /**
- * The class represents an expected minimum gold holding.
+ * The class represents a gold goal for the player. To
+ * achieve the goal, the player must obtain a minimum
+ * amount of gold.
  *
- * @author ...
- * @version JDK 17
+ * @author Ramtin Samavat and Tobias Oftedal.
+ * @version 1.0
+ * @since April 23, 2023.
  */
 public class GoldGoal implements Goal {
   private final int minimumGold;
@@ -46,5 +50,33 @@ public class GoldGoal implements Goal {
       throw new NullPointerException("Player cannot be null");
     }
     return player.getGold() > getMinimumGold();
+  }
+
+  /**
+   * The method checks for equality between GoldGoal objects.
+   *
+   * @param o the object to which it is being compared.
+   * @return a boolean value which indicate whether they are equal or not.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    GoldGoal goldGoal = (GoldGoal) o;
+    return getMinimumGold() == goldGoal.getMinimumGold();
+  }
+
+  /**
+   * The method generates a hash value for the object.
+   *
+   * @return hash value for the object.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(getMinimumGold());
   }
 }
