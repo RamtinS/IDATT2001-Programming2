ef16aa3a0879280f2afdb4e45ee9ddd93080dadb
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-04-26 14:08:04 +0200

    Add more exception handling to FilePathValidator class method
    Add exception handling for file extension in the validatePathOfFile method in the FilePathValidator class.

--- src/main/java/edu/ntnu/idatt2001/paths/filehandling/FilePathValidator.java
+++ src/main/java/edu/ntnu/idatt2001/paths/filehandling/FilePathValidator.java
@@ -14,11 +14,14 @@ public class FilePathValidator {
    *
    * @param pathOfFile the path of the file to read/write to.
    * @param FILE_EXTENSION the file extension that the path should end with.
-   * @throws NullPointerException if the pathOfFile is null.
+   * @throws NullPointerException if the pathOfFile or FILE_EXTENSION is null.
    * @throws IllegalArgumentException if pathOfFile is blank or does not end with FILE_EXTENSION.
    */
   public static void validatePathOfFile(String pathOfFile, String FILE_EXTENSION)
           throws NullPointerException, IllegalArgumentException {
+    if (FILE_EXTENSION == null) {
+      throw new NullPointerException("The file extension cannot be null.");
+    }
     if (pathOfFile == null) {
       throw new NullPointerException("The path of the file cannot be null.");
     }
