d9536b8b3e266adb2f7980b545068c9869b117cd
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-05-12 15:20:06 +0200

    Create StoryCreator class - Enable a graphical way of creating a story. - Create methods for creating passages and lines visually. - Add storyCreatorListener to activate button actions.

---
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/listeners/StoryCreatorListener.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2001.paths.gui.listeners;
+
+/**
+ * Interface for setting actions to the buttons of a "main menu" frame.
+ *
+ * @author Ramtin Samavat and Tobias Oftedal.
+ * @version 1.0
+ * @since April 26, 2023.
+ */
+public interface StoryCreatorListener {
+  void onReturnClicked();
+}
