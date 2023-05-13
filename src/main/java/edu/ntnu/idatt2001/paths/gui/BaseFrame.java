d3c632edbc45cbe1ee96ba5f040bafebd7512522
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-13 14:49:47 +0200

    Add logger in applyBackground method in BaseFrame class

--- src/main/java/edu/ntnu/idatt2001/paths/gui/BaseFrame.java
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/BaseFrame.java
@@ -1,12 +1,14 @@
 package edu.ntnu.idatt2001.paths.gui;
 
-
 import edu.ntnu.idatt2001.paths.Link;
 import edu.ntnu.idatt2001.paths.Passage;
 import edu.ntnu.idatt2001.paths.Player;
 import edu.ntnu.idatt2001.paths.gui.listeners.BaseFrameListener;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
@@ -20,8 +22,9 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 /**
- * Class representing a frame for the game. Shows the passage text of the current passage and has
- * buttons for the links that the user can follow.
+ * The BaseFrame class representing a frame for the game.
+ * It shows the passage text of the current passage and has
+ * buttons for the links that the player can follow.
  *
  * @author Ramtin Samavat and Tobias Oftedal.
  * @version 1.0
@@ -29,6 +32,7 @@ import javafx.scene.text.Text;
  */
 public class BaseFrame extends AnchorPane {
 
+  private static final Logger logger = Logger.getLogger(BaseFrame.class.getName());
   private final double width;
   private final double height;
   private final Player player;
@@ -45,7 +49,7 @@ public class BaseFrame extends AnchorPane {
    * @param listener The listener used to send out button activations from the frame.
    */
   public BaseFrame(Passage passage, Player player, double width, double height,
-                   BaseFrameListener listener) {
+                   BaseFrameListener listener) throws NullPointerException {
     Objects.requireNonNull(passage, "Passage cannot be null");
     Objects.requireNonNull(player, "Player cannot be null");
     Objects.requireNonNull(listener, "Listener cannot be null");
@@ -67,31 +71,36 @@ public class BaseFrame extends AnchorPane {
   }
 
   /**
-   * Applies a background if any jpg in the resources/ImageFiles match the passage name.
+   * The method applies a background image to the current scene.
    */
   private void applyBackground() {
-
     try {
-      Image image = new Image("ImageFiles/" + passage.getTitle() + ".jpg");
+      String pathOfFile = "src/main/resources/images/"
+              + passage.getTitle().toLowerCase().replace(" ", "_") + ".png";
 
-      BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT,
-          BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
-          new BackgroundSize(1, 1, true, true, false, false));
+      Image image = new Image(pathOfFile);
+
+      BackgroundSize backgroundSize = new BackgroundSize(1, 1, true, true, false, false);
+
+      BackgroundImage backgroundImage = new BackgroundImage(image,
+              BackgroundRepeat.NO_REPEAT,
+              BackgroundRepeat.NO_REPEAT,
+              BackgroundPosition.CENTER,
+              backgroundSize);
 
       Background background = new Background(backgroundImage);
-
       setBackground(background);
-    } catch (Exception e) {
-      // setStyle("-fx-background-color: #00FF00");
+    } catch(Exception e) {
+      logger.log(Level.WARNING, "Failed to load background image: " + e.getMessage(), e);
+      Alert alert = new Alert(Alert.AlertType.WARNING, "Failed to load background image.");
+      alert.showAndWait();
     }
-
   }
 
   /**
    * Sets a health bar in the top left corner, showing how much health the user has left.
    */
   private void setHealthBar() {
-
     ProgressBar healthBar = new ProgressBar((double) player.getHealth() / 100);
 
     healthBar.prefWidthProperty().bind(widthProperty().divide(5));
@@ -155,21 +164,18 @@ public class BaseFrame extends AnchorPane {
    */
   private void addLinkButtons() {
     VBox choiceButtons = new VBox();
-    try {
-      for (Link link : passage.getLinks()) {
 
-        Button button = new Button(link.getText());
-        button.setOnAction(event -> listener.onOptionButtonClicked(link));
-        button.setPrefWidth(width / 10);
-        choiceButtons.getChildren().add(button);
-      }
+    for (Link link : passage.getLinks()) {
 
-      choiceButtons.setLayoutX(width / 1.5);
-      choiceButtons.setLayoutY(height - height / 3);
-      getChildren().add(choiceButtons);
-    } catch (Exception e) {
-      e.printStackTrace();
+      Button button = new Button(link.getText());
+      button.setOnAction(event -> listener.onOptionButtonClicked(link));
+      button.setPrefWidth(width / 10);
+      choiceButtons.getChildren().add(button);
     }
+
+    choiceButtons.setLayoutX(width / 1.5);
+    choiceButtons.setLayoutY(height - height / 3);
+    getChildren().add(choiceButtons);
   }
 
   /**
@@ -187,14 +193,12 @@ public class BaseFrame extends AnchorPane {
     attributes.setLayoutY(height / 3);
     attributes.setStyle("-fx-background-color: #C5B1FF");
     getChildren().add(attributes);
-
   }
 
   /**
    * Adds the passage text to the frame.
    */
   private void showPassageText() {
-
     Text text = new Text(passage.getContent());
     text.setWrappingWidth(width / 10);
     VBox textPane = new VBox();
@@ -225,7 +229,5 @@ public class BaseFrame extends AnchorPane {
     inventoryPane.setLayoutY(getPrefHeight() - 70);
     inventoryPane.setLayoutX(10);
   }
-
-
 }
 
