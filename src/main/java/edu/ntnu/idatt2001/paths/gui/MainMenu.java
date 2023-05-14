dd63c65cc76ccce5c41a3cffd10b05e7c3b48126
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-05-14 15:07:56 +0200

    Refactor MainMenu class - Make everything resize better - Make headline centered while still resizing - Add troll to background

--- src/main/java/edu/ntnu/idatt2001/paths/gui/MainMenu.java
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/MainMenu.java
@@ -2,16 +2,24 @@ package edu.ntnu.idatt2001.paths.gui;
 
 
 import edu.ntnu.idatt2001.paths.gui.listeners.MainMenuListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.TranslateTransition;
 import javafx.beans.value.ChangeListener;
-import javafx.geometry.VPos;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
 
 /**
  * Represents the main menu of the application, with options for creating a new game, loading a
@@ -23,8 +31,15 @@ import javafx.scene.text.TextAlignment;
  */
 public class MainMenu extends Pane {
 
-  private final double width;
-  private final double height;
+  Button newGame;
+  Button loadGame;
+  Button tutorial;
+  Button createGame;
+  private static final double BUTTON_WIDTH_PERCENTAGE = 0.2;
+  private static final double BUTTON_BOX_HEIGHT_PERCENTAGE = 0.5;
+  private static final double BUTTON_SPACING_PERCENTAGE = 0.05;
+  private static final Logger LOGGER = Logger.getLogger(MainMenu.class.getName());
+
   private final MainMenuListener mainMenuListener;
 
   /**
@@ -34,16 +49,15 @@ public class MainMenu extends Pane {
    * @param height   The height of the menu.
    * @param listener The l
    */
-  public MainMenu(int width, int height, MainMenuListener listener) {
-    this.width = width;
-    this.height = height;
+  public MainMenu(double width, double height, MainMenuListener listener) {
+    DimensionUtility.changeAllPaneWidths(this, width);
+    DimensionUtility.changeAllPaneHeights(this, height);
     this.mainMenuListener = listener;
-
-    setPrefHeight(height);
-    setPrefWidth(width);
 
     setHeadLine();
     setOptionButtons();
+    setBackground();
+    addTroll();
 
   }
 
@@ -52,94 +66,134 @@ public class MainMenu extends Pane {
    */
   private void setHeadLine() {
 
-    Text headLine = new Text("Main menu");
-    headLine.setFont(Font.font("Arial", 30));
+    HBox headLine = new HBox();
 
-    headLine.setWrappingWidth(width / 2);
-    headLine.setTextOrigin(VPos.CENTER);
-
-    headLine.xProperty().bind(widthProperty().subtract(headLine.getWrappingWidth()).divide(2));
-    headLine.yProperty().bind(heightProperty().multiply(0.1));
+    Label text = new Label("    Paths    ");
+    headLine.getChildren().add(text);
+    text.setFont(Font.font("Arial", 45));
+    text.setTextAlignment(TextAlignment.RIGHT);
+    headLine.setAlignment(Pos.CENTER);
+    headLine.layoutYProperty().bind(heightProperty().multiply(0.1));
 
     ChangeListener<Number> headLineHeightListener = (value, oldValue, newValue) -> {
-      headLine.setFont(
+      text.setFont(
           Font.font(Math.min(heightProperty().getValue() / 10, widthProperty().getValue() / 20)));
+      headLine.setPrefWidth(widthProperty().get());
 
     };
+
     heightProperty().addListener(headLineHeightListener);
     widthProperty().addListener(headLineHeightListener);
-
-    headLine.setTextAlignment(TextAlignment.CENTER);
 
     getChildren().add(headLine);
   }
 
   /**
-   * Adds buttons for all user options.
+   * Adds buttons for all user options to the pane.
+   * <li>"New game" button: triggers {@link MainMenuListener#onNewGameClicked()}</li>
+   * <li>"Load game" button: triggers {@link MainMenuListener#onLoadGameClicked()} ()}</li>
+   * <li>"Tutorial" button: triggers {@link MainMenuListener#onTutorialButtonClicked()} ()}</li>
+   * <li>"Create game" button: triggers {@link MainMenuListener#onCreateStoryMenuClicked()}
+   * ()}</li>
    */
   private void setOptionButtons() {
     final VBox buttonBox = new VBox();
-    Button newGame = new Button("New game");
-    Button loadGame = new Button("Load game");
-    Button tutorial = new Button("Tutorial");
-    Button createGame = new Button("Create story");
+    newGame = new Button("New Game");
+    loadGame = new Button("Load Game");
+    tutorial = new Button("Tutorial");
+    createGame = new Button("Create Story");
 
-    List<Button> buttonsToAdd = new ArrayList<>();
-    buttonsToAdd.add(newGame);
-    buttonsToAdd.add(loadGame);
-    buttonsToAdd.add(tutorial);
-    buttonsToAdd.add(createGame);
+    widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> {
+      DimensionUtility.changeAllPaneWidths(buttonBox,
+          newSceneWidth.doubleValue() * BUTTON_WIDTH_PERCENTAGE);
+      buttonBox.setLayoutX((getWidth() - buttonBox.getPrefWidth()) / 2);
+    });
+
+    heightProperty().addListener((observableValue, oldSceneWidth, newSceneHeight) -> {
+      changeButtonBoxHeight(buttonBox, newSceneHeight);
+    });
+
+    newGame.setPrefWidth(Double.MAX_VALUE);
+    loadGame.setPrefWidth(Double.MAX_VALUE);
+    tutorial.setPrefWidth(Double.MAX_VALUE);
+    createGame.setPrefWidth(Double.MAX_VALUE);
 
     newGame.setOnAction(event -> mainMenuListener.onNewGameClicked());
     loadGame.setOnAction(event -> mainMenuListener.onLoadGameClicked());
     tutorial.setOnAction(event -> mainMenuListener.onTutorialButtonClicked());
     createGame.setOnAction(event -> mainMenuListener.onCreateStoryMenuClicked());
 
-    ChangeListener<Number> widthListener = getButtonWidthChangeListener(buttonsToAdd);
-    ChangeListener<Number> heightListener = getButtonHeightChangeListener(buttonsToAdd);
-
-    widthProperty().addListener(widthListener);
-    heightProperty().addListener(heightListener);
     buttonBox.getChildren().addAll(newGame, loadGame, tutorial, createGame);
-    buttonBox.layoutXProperty().bind(widthProperty().subtract(newGame.widthProperty()).divide(2));
-    buttonBox.layoutYProperty().bind(heightProperty().subtract(newGame.heightProperty()).divide(2));
-
     getChildren().add(buttonBox);
 
   }
 
   /**
-   * Changes the button size for the added buttons when the change listener detects a change.
+   * Changes the height of all the buttons to the given height.
    *
-   * @param buttonsToAdd The buttons change size for when the listener detects a change.
-   * @return The ChangeListener created, that will change button sizes when detecting a change.
+   * @param buttonBox The Vbox that contains the buttons.
+   * @param newHeight The new height of the buttons.
    */
-  private ChangeListener<Number> getButtonWidthChangeListener(List<Button> buttonsToAdd) {
-    return (value, oldValue, newValue) -> {
-      for (Button button : buttonsToAdd) {
-        double newWidth = widthProperty().getValue() / 7;
-        button.setPrefWidth(newWidth);
-        button.setFont(Font.font(newWidth / 8));
-      }
-    };
+  private void changeButtonBoxHeight(VBox buttonBox, Number newHeight) {
+    DimensionUtility.changeAllPaneHeights(buttonBox,
+        newHeight.doubleValue() * BUTTON_BOX_HEIGHT_PERCENTAGE);
+    buttonBox.setLayoutY((getHeight() - buttonBox.getPrefHeight()) / 2);
+    double spacing = buttonBox.getPrefHeight() * BUTTON_SPACING_PERCENTAGE;
+    buttonBox.setSpacing(spacing);
+    double buttonHeight = (buttonBox.getPrefHeight() - spacing * 3) / 4;
+    DimensionUtility.changeAllButtonHeights(newGame, buttonHeight);
+    DimensionUtility.changeAllButtonHeights(loadGame, buttonHeight);
+    DimensionUtility.changeAllButtonHeights(tutorial, buttonHeight);
+    DimensionUtility.changeAllButtonHeights(createGame, buttonHeight);
   }
 
   /**
-   * Changes the button size for the added buttons when the change listener detects a change.
-   *
-   * @param buttonsToAdd The buttons change size for when the listener detects a change.
-   * @return The ChangeListener created, that will change button sizes when detecting a change.
+   * Sets the image background to a picture of a forest.
    */
-  private ChangeListener<Number> getButtonHeightChangeListener(List<Button> buttonsToAdd) {
-    return (value, oldValue, newValue) -> {
-      for (Button button : buttonsToAdd) {
-        double newHeight = heightProperty().getValue() / 14;
-        button.setPrefHeight(newHeight);
-        button.setMaxHeight(newHeight);
-        button.setMinHeight(newHeight);
-        button.setFont(Font.font(newHeight / 2));
+  private void setBackground() {
+    try {
+      Image image = new Image("images/forestadventure/beginnings.png");
+
+      BackgroundImage backgroundImage = new BackgroundImage(image, null, null, null, null);
+      Background background = new Background(backgroundImage);
+
+      setBackground(background);
+    } catch (Exception e) {
+      LOGGER.log(Level.INFO, "Could not add background because" + e.getMessage());
+    }
+
+  }
+
+  /**
+   * Adds an image of a troll to the right side of the menu.
+   */
+  private void addTroll() {
+    ImageView troll = new ImageView(new Image("images/troll_transparent.png"));
+    ChangeListener<Number> heightListener = new ChangeListener() {
+      @Override
+      public void changed(ObservableValue observableValue, Object o, Object t1) {
+        troll.setFitHeight(heightProperty().get() / 2);
+        troll.setLayoutY(heightProperty().get() - troll.fitHeightProperty().get());
       }
     };
+    ChangeListener<Number> widthListener = new ChangeListener() {
+      @Override
+      public void changed(ObservableValue observableValue, Object o, Object t1) {
+        troll.setFitWidth(widthProperty().get() / 3);
+        troll.setLayoutX(widthProperty().get() - troll.fitWidthProperty().get());
+      }
+    };
+    heightProperty().addListener(heightListener);
+    widthProperty().addListener(widthListener);
+    troll.setPreserveRatio(true);
+
+    getChildren().add(troll);
+    TranslateTransition transition = new TranslateTransition(Duration.seconds(5), troll);
+    transition.setByY(-100);
+    transition.setCycleCount(TranslateTransition.INDEFINITE);
+    transition.setAutoReverse(true);
+    transition.play();
   }
+
 
 }
