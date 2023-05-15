d621f8df4a76cfae9bf9d00c238fe93040195d0f
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-15 22:46:57 +0200

    Create unit test for ActionType enum class

---
+++ src/test/java/edu/ntnu/idatt2001/paths/actions/ActionTypeTest.java
@@ -0,0 +1,47 @@
+package edu.ntnu.idatt2001.paths.actions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActionTypeTest {
+
+  @Test
+  @DisplayName("Test get action value description")
+  void testGetActionValueDescription() {
+    assertEquals("gold", ActionType.GOLD.getActionValueDescription());
+    assertEquals("health", ActionType.HEALTH.getActionValueDescription());
+    assertEquals("item", ActionType.INVENTORY.getActionValueDescription());
+    assertEquals("points", ActionType.SCORE.getActionValueDescription());
+  }
+
+  @Test
+  @DisplayName("Test get action class")
+  void testGetActionClass() {
+    assertEquals(GoldAction.class, ActionType.GOLD.getActionClass());
+    assertEquals(HealthAction.class, ActionType.HEALTH.getActionClass());
+    assertEquals(InventoryAction.class, ActionType.INVENTORY.getActionClass());
+    assertEquals(ScoreAction.class, ActionType.SCORE.getActionClass());
+  }
+
+  @Test
+  @DisplayName("Should get action type from action description")
+  void shouldGetActionTypeFromActionDescription() {
+    ActionType goldActionType = ActionType.getActionTypeFromActionDescription("gold");
+    assertEquals(ActionType.GOLD, goldActionType);
+    ActionType healthActionType = ActionType.getActionTypeFromActionDescription("health");
+    assertEquals(ActionType.HEALTH, healthActionType);
+    ActionType inventoryAction = ActionType.getActionTypeFromActionDescription("inventory");
+    assertEquals(ActionType.INVENTORY, inventoryAction);
+    ActionType scoreAction = ActionType.getActionTypeFromActionDescription("score");
+    assertEquals(ActionType.SCORE, scoreAction);
+  }
+
+  @Test
+  @DisplayName("Should not get action type from action description throws IllegalArgumentException")
+  void shouldNotGetActionTypeFromActionDescriptionThrowsIllegalArgumentException() {
+    assertThrows(IllegalArgumentException.class, () -> ActionType.getActionTypeFromActionDescription("invalid"));
+    assertThrows(IllegalArgumentException.class, () -> ActionType.getActionTypeFromActionDescription(null));
+  }
+}
+
