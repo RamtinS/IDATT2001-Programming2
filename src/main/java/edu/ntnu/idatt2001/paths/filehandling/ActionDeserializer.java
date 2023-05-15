fe5ce1507052b0dda06574772a03499927dccae0
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-15 22:49:13 +0200

    Refactor deserialize method in ActionDeserializer class
    Refactor the method to use ActionType enum for determining Action objects instead of using hardcoded if-else statements for each action type.

--- src/main/java/edu/ntnu/idatt2001/paths/filehandling/ActionDeserializer.java
+++ src/main/java/edu/ntnu/idatt2001/paths/filehandling/ActionDeserializer.java
@@ -6,10 +6,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import edu.ntnu.idatt2001.paths.actions.Action;
-import edu.ntnu.idatt2001.paths.actions.GoldAction;
-import edu.ntnu.idatt2001.paths.actions.HealthAction;
-import edu.ntnu.idatt2001.paths.actions.InventoryAction;
-import edu.ntnu.idatt2001.paths.actions.ScoreAction;
+import edu.ntnu.idatt2001.paths.actions.ActionType;
 import java.lang.reflect.Type;
 
 /**
@@ -19,31 +16,30 @@ import java.lang.reflect.Type;
  *
  * @author Ramtin Samavat and Tobias Oftedal.
  * @version 1.0
- * @since April 23, 2023.
+ * @since May 15, 2023.
  */
 public class ActionDeserializer implements JsonDeserializer<Action> {
 
   /**
    * The method deserializes a JSON element into its corresponding Action object.
+   * The method only supports Action objects determined by the ActionType enum.
    *
    * @param jsonElement the JSON element to be deserialized.
    * @param type the type of the object to be deserialized to.
    * @param context the context of the deserialization.
    * @return the corresponding Action object.
-   * @throws JsonParseException if JSON element cannot be deserialized because of unknown action type.
+   * @throws JsonParseException if JsonElement cannot be deserialized because of unknown action type.
    */
   @Override
   public Action deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context)
           throws JsonParseException {
+
     JsonObject jsonObject = jsonElement.getAsJsonObject();
-    if (jsonObject.has("gold")) {
-      return context.deserialize(jsonElement, GoldAction.class);
-    } else if (jsonObject.has("health")) {
-      return context.deserialize(jsonElement, HealthAction.class);
-    } else if (jsonObject.has("item" )) {
-      return context.deserialize(jsonElement, InventoryAction.class);
-    } else if (jsonObject.has("points")) {
-      return context.deserialize(jsonElement, ScoreAction.class);
+    for (ActionType actionType : ActionType.values()) {
+      if (jsonObject.has(actionType.getActionValueDescription())) {
+        Class<? extends Action> actionClass = actionType.getActionClass();
+        return context.deserialize(jsonElement, actionClass);
+      }
     }
     throw new JsonParseException("Unknown action type: " + jsonElement);
   }
