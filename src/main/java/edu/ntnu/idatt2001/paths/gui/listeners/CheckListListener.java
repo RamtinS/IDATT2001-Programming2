7b12ea0b5658480305a1540391f7ea2656a36179
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-05-18 10:34:54 +0200

    Create the CheckListView class
    - Lets the user select between a list of given items.
    - Add a CheckListListener to enable button activity.

---
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/listeners/CheckListListener.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idatt2001.paths.gui.listeners;
+
+
+import java.util.List;
+
+/**
+ * Interface for setting actions to the buttons of a base frame.
+ *
+ * @author Tobias Oftedal
+ * @author Ramtin Samavat
+ * @version 1.0
+ * @since May 16, 2023.
+ */
+public interface CheckListListener {
+
+  void onConfirmSelectionsClicked(List<String> selectedGoals);
+
+}
+
