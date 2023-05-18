ca4b28ac1a90b05c27ffc067ad327f59a404cce6
Tobias Skipevåg Oftedal <tobiasof@stud.ntnu.no>
2023-05-18 23:14:43 +0200

    Refactor LoadStoredGamesMenu.java
    - Add headline
    - Fix delete functionality
    - Add padding

--- src/main/java/edu/ntnu/idatt2001/paths/gui/menus/LoadStoredGamesMenu.java
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/menus/LoadStoredGamesMenu.java
@@ -1,23 +1,36 @@
-package edu.ntnu.idatt2001.paths.gui;
+package edu.ntnu.idatt2001.paths.gui.menus;
 
-import edu.ntnu.idatt2001.paths.Game;
-import edu.ntnu.idatt2001.paths.Link;
-import edu.ntnu.idatt2001.paths.Story;
 import edu.ntnu.idatt2001.paths.controller.GameManager;
 import edu.ntnu.idatt2001.paths.gui.listeners.LoadStoredGamesListener;
+import edu.ntnu.idatt2001.paths.model.Game;
+import edu.ntnu.idatt2001.paths.model.Link;
+import edu.ntnu.idatt2001.paths.model.Story;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 /**
  * Represents a menu used for loading stored games.
@@ -27,12 +40,15 @@ import javafx.scene.layout.Pane;
  * @version 1.0
  * @since May 12, 2023.
  */
-public class LoadStoredGamesMenu extends Pane {
+public class LoadStoredGamesMenu extends BorderPane {
 
   private final LoadStoredGamesListener listener;
   private final GameManager gameManager;
   private TableView<Game> gameTable;
   private List<Game> gameList;
+  private VBox rightButtonPane;
+  private VBox leftButtonPane;
+  private static final Logger LOGGER = Logger.getLogger(LoadStoredGamesMenu.class.getName());
 
   /**
    * Constructor for a LoadStoredGamesMenu object. Adds a table with selectable games.
@@ -48,14 +64,30 @@ public class LoadStoredGamesMenu extends Pane {
       throw new IllegalArgumentException("Listener cannot be null");
     }
 
+    rightButtonPane = new VBox();
+    rightButtonPane.setSpacing(100);
+    rightButtonPane.setPadding(new Insets(0, 0, 0, 40));
+    rightButtonPane.setAlignment(Pos.CENTER);
+    rightButtonPane.setPrefWidth(getPrefWidth() * 0.2);
+
+    leftButtonPane = new VBox();
+    leftButtonPane.setPadding(new Insets(0, 40, 0, 0));
+    setLeft(leftButtonPane);
+    setRight(rightButtonPane);
+
+    setBackgroundImage("images/forestadventure/beginnings.png");
+
     this.gameManager = GameManager.getInstance();
     this.listener = listener;
+    setHeadline("Saved games");
     setPrefWidth(width);
     setPrefHeight(height);
     fillGamesData();
     addGameTable();
     addButtons();
-    setStyle("-fx-background-color: #000000");
+    setPadding(new Insets(20));
+
+    setMinWidth(500);
   }
 
   /**
@@ -69,8 +101,9 @@ public class LoadStoredGamesMenu extends Pane {
    */
   private void addButtons() {
     addReturnButton();
-    addConfirmButton();
     addDeleteButton();
+    addConfirmButton();
+
   }
 
   /**
@@ -84,12 +117,16 @@ public class LoadStoredGamesMenu extends Pane {
    */
   private void addDeleteButton() {
     Button deleteButton = new Button("Delete");
-    getChildren().add(deleteButton);
-    deleteButton.setLayoutX(200);
+    rightButtonPane.getChildren().add(deleteButton);
+    deleteButton.setPrefWidth(Double.MAX_VALUE);
+
+
     deleteButton.setOnAction(event -> {
       Game selectedGame = gameTable.getSelectionModel().getSelectedItem();
       try {
         gameManager.deleteGame(selectedGame);
+        fillGamesData();
+        addGameTable();
       } catch (IOException | IllegalArgumentException | NullPointerException e) {
         new Alert(AlertType.ERROR,
             "Could not delete game because: " + e.getMessage()).showAndWait();
@@ -104,7 +141,6 @@ public class LoadStoredGamesMenu extends Pane {
    * <p>
    * <li>The button activation triggers the
    * {@link LoadStoredGamesListener#onSelectedGameClicked(Game)} method.</li>
-   * <li></li>
    * </p>
    */
   private void addConfirmButton() {
@@ -121,16 +157,27 @@ public class LoadStoredGamesMenu extends Pane {
     });
 
     confirmButton.disableProperty().bind(gameIsNotSelected());
-    confirmButton.setLayoutX(100);
-    getChildren().add(confirmButton);
+    confirmButton.setMinWidth(100);
+    HBox.setHgrow(rightButtonPane, Priority.NEVER);
+
+    rightButtonPane.getChildren().add(confirmButton);
   }
 
   private void addReturnButton() {
-    Button returnButton = new Button("RETURN");
+    Button returnButton = new Button("Return");
+    returnButton.setId("return-button");
     returnButton.setOnAction(event -> listener.onReturnClicked());
-    returnButton.setLayoutX(0);
-    returnButton.setLayoutY(0);
-    getChildren().add(returnButton);
+    leftButtonPane.getChildren().add(returnButton);
+  }
+
+  private void setHeadline(String headline){
+    Label headLabel = new Label(headline);
+    headLabel.setFont(new Font(40));
+    headLabel.setId("headline-background");
+    setTop(headLabel);
+    setAlignment(headLabel, Pos.CENTER);
+    headLabel.setTextAlignment(TextAlignment.CENTER);
+    headLabel.setPadding(new Insets(0, 30, 0, 30));
   }
 
   /**
@@ -147,6 +194,7 @@ public class LoadStoredGamesMenu extends Pane {
 
     gameTable = new TableView<>();
     gameTable.getColumns().clear();
+
 
     TableColumn<Game, String> gameIdColumn = new TableColumn<>();
     gameIdColumn.setCellValueFactory(
@@ -168,6 +216,7 @@ public class LoadStoredGamesMenu extends Pane {
         String.valueOf(cellData.getValue().getStory().getBrokenLinks().size())));
     brokenLinksColumn.setText("Broken links");
 
+
     gameTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
     gameTable.setStyle("-fx-border-color: #000000");
     gameTable.getColumns().add(gameIdColumn);
@@ -175,9 +224,12 @@ public class LoadStoredGamesMenu extends Pane {
     gameTable.getColumns().add(storyColumn);
     gameTable.getColumns().add(brokenLinksColumn);
     gameTable.setItems(FXCollections.observableList(gameList));
-    gameTable.setPrefWidth(400);
-    gameTable.setLayoutX((getPrefWidth() - gameTable.getPrefWidth()) / 2);
-    getChildren().add(gameTable);
+    gameTable.setPrefWidth(getPrefWidth() * 0.5);
+    gameTable.setMinWidth(getPrefWidth() * 0.3);
+
+    gameTable.setPrefHeight(getPrefHeight() * 0.8);
+
+    setCenter(gameTable);
   }
 
   /**
@@ -218,6 +270,24 @@ public class LoadStoredGamesMenu extends Pane {
 
     }
     return true;
+  }
+
+
+
+
+
+  private void setBackgroundImage(String imagePath) {
+    try {
+      Image image = new Image(imagePath);
+
+      BackgroundImage backgroundImage = new BackgroundImage(image, null, null, null, null);
+      Background background = new Background(backgroundImage);
+
+      setBackground(background);
+    } catch (Exception e) {
+      LOGGER.log(Level.INFO, "Could not add background because" + e.getMessage());
+    }
+
   }
 
 }
