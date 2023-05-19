7b0519345914d741da92e42a0db81d493d730c34
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-19 22:29:20 +0200

    Refactor CreateGameListener interface
    Add new parameter pathOfStoryFile to the onCreateClicked method in CreateGameListener interface and update the JavaDoc in the class.

--- src/main/java/edu/ntnu/idatt2001/paths/gui/listeners/CreateGameListener.java
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/listeners/CreateGameListener.java
@@ -1,21 +1,37 @@
 package edu.ntnu.idatt2001.paths.gui.listeners;
 
 import edu.ntnu.idatt2001.paths.Difficulty;
-import edu.ntnu.idatt2001.paths.model.Story;
 import edu.ntnu.idatt2001.paths.goals.Goal;
+import edu.ntnu.idatt2001.paths.model.Story;
 import java.util.List;
 
 /**
  * Interface for setting actions to the buttons of a "create game" frame.
  *
- * @author Ramtin Samavat and Tobias Oftedal.
+ * @author Tobias Oftedal.
+ * @author Ramtin Samavat.
  * @version 1.0
  * @since April 26, 2023.
  */
 public interface CreateGameListener {
 
+  /**
+   * The method handles the return action
+   * and is called when the return button is clicked.
+   */
   void onReturnClicked();
 
-  void onCreateClicked(List<Goal> chosenGoals, String gameId, String playerName, Difficulty chosenDifficulty,
-                       Story selectedStory);
+  /**
+   * The method handles the creation action
+   * and is called when the create button is clicked.
+   *
+   * @param pathOfStoryFile the path of the story file.
+   * @param chosenGoals the list of chosen goals for the game.
+   * @param gameId the ID of the game.
+   * @param playerName the name of the player.
+   * @param chosenDifficulty the chosen difficulty level for the game.
+   * @param selectedStory the selected story for the game.
+   */
+  void onCreateClicked(String pathOfStoryFile, List<Goal> chosenGoals, String gameId,
+                       String playerName, Difficulty chosenDifficulty, Story selectedStory);
 }
