c513d77744ab76f918c57dfeb95c7ca145cb98b6
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-20 23:25:46 +0200

    Rename LoadStoredGamesListener interface to StoredGamesListener
    Update interface name from LoadStoredGamesListener to StoredGamesListener for improved clarity and consistency.

--- src/main/java/edu/ntnu/idatt2001/paths/gui/listeners/StoredGamesListener.java
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/listeners/StoredGamesListener.java
@@ -9,10 +9,17 @@ import edu.ntnu.idatt2001.paths.model.Game;
  * @version 1.0
  * @since May 12, 2023.
  */
-public interface LoadStoredGamesListener {
+public interface StoredGamesListener {
 
+  /**
+   * The method handles the selected game action.
+   */
   void onSelectedGameClicked(Game game);
 
+  /**
+   * The method handles the return action
+   * and is called when the return button is clicked.
+   */
   void onReturnClicked();
 
 }
