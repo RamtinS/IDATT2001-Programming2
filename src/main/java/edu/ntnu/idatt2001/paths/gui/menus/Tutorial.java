7cd4476ebb471466cf6c5bb3a4d2bdd9f0919cec
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-21 01:45:57 +0200

    Create new class Tutorial
    Create class for displaying the game tutorial.

---
+++ src/main/java/edu/ntnu/idatt2001/paths/gui/menus/Tutorial.java
@@ -0,0 +1,41 @@
+package edu.ntnu.idatt2001.paths.gui.menus;
+
+import edu.ntnu.idatt2001.paths.gui.listeners.TutorialListener;
+import edu.ntnu.idatt2001.paths.gui.utility.GuiUtils;
+import java.util.Objects;
+import javafx.geometry.Pos;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * The Tutorial class displays the game tutorial.
+ *
+ * @author Ramtin Samavat
+ * @author Tobias Oftedal
+ * @version 1.0
+ * @since May 20, 2023.
+ */
+public class Tutorial extends BorderPane {
+
+  /**
+   * Constructs a Tutorial object with the specified width, height, and listener.
+   *
+   * @param width the preferred width of the tutorial.
+   * @param height the preferred height of the tutorial.
+   * @param listener the listener for tutorial events.
+   * @throws NullPointerException if the listener is null.
+   */
+  public Tutorial(double width, double height, TutorialListener listener)
+          throws NullPointerException {
+
+    TutorialListener tutorialListener = Objects.requireNonNull(listener,
+            "TutorialListener cannot be null.");
+
+    GuiUtils.setBackgroundImage(this, "images/forestadventure/beginnings.png");
+    GuiUtils.setHeadline(this, "Tutorial", 40, 0, 30, 0, 30);
+    GuiUtils.createReturnButton(this, "Return to main menu",
+            tutorialListener::onReturnClicked, Pos.CENTER);
+
+    setPrefWidth(width);
+    setPrefHeight(height);
+  }
+}
