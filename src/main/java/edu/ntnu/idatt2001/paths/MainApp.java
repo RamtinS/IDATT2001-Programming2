bc42769757f0280917e84a6d2fe5dfc106f97403
Ramtin Forouzandehjoo Samavat <ramtinfs@stud.ntnu.no>
2023-05-21 21:59:58 +0200

    Create MainApp class
    The MainApp class is responsible for running the main application.
    Add the MainApp class to the pom file.

---
+++ src/main/java/edu/ntnu/idatt2001/paths/MainApp.java
@@ -0,0 +1,23 @@
+package edu.ntnu.idatt2001.paths;
+
+import edu.ntnu.idatt2001.paths.view.PathsApp;
+
+/**
+ * The MainApp class is responsible for running the main application.
+ * It serves as the entry point for launching the PathsApp.
+ *
+ * @author Ramtin Samavat and Tobias Oftedal.
+ * @version 1.0
+ * @since May 21, 2023.
+ */
+public class MainApp {
+
+  /**
+   * The method lunches the application.
+   *
+   * @param args an array of command-line arguments for the application.
+   */
+  public static void main(String[] args) {
+    PathsApp.main(args);
+  }
+}
